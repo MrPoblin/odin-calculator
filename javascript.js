dddf845d8b77e715a6311d6950694a036dc58664
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2024-10-27 13:49:38 +0200

    Finish Extended Features

--- javascript.js
+++ javascript.js
@@ -38,10 +38,10 @@ function operate(num1, num2, op){
             break;
     }
     if(op){
-        calculator.number2 = 0;
-        display.textContent = calculator.number1;
+        calculator.number2 = "0";
+        setDisplay(calculator.number1);
             if(calculator.number1 === "bruh"){
-                calculator.number1 = 0;
+                calculator.number1 = "0";
             }
      }
 }
@@ -49,20 +49,28 @@ function operate(num1, num2, op){
 display.textContent = 0;
 
 let calculator = {
-    number1: 0,
-    number2: 0,
+    number1: "0",
+    number2: "0",
     operator: 0,
 }
 
 let lastClicked = 0;
 
 function setDisplay(value){
-    display.textContent = value;
+    if(calculator.number1 === "bruh"){
+        display.textContent = calculator.number1;
+    }
+    else{
+        if(parseFloat(value) % 1 > 0){
+            value = Math.round(value * 1000000) / 1000000;
+        }
+        display.textContent = value;
+    }
 }
 
 function addNumber(numSlot, value){
-    if(calculator[numSlot]){
-        calculator[numSlot] = calculator[numSlot] * 10 + value;
+    if(calculator[numSlot] != 0){
+        calculator[numSlot] = calculator[numSlot] + value;
     }
     else{
         calculator[numSlot] = value;
@@ -70,20 +78,30 @@ function addNumber(numSlot, value){
     setDisplay(calculator[numSlot]);
 }
 
+function delNumber(numSlot){
+    calculator[numSlot] = calculator[numSlot].slice(0, -1);
+    setDisplay(calculator[numSlot]);
+}
+
+function addDot(numSlot){
+    if (!calculator[numSlot].includes('.')){
+        calculator[numSlot] += '.';
+        setDisplay(calculator[numSlot]);
+    }
+}
 
 function buttonClicked(e){
-    console.log(lastClicked);
     let content = e.srcElement.textContent;
     if(parseInt(content) || parseInt(content) === 0){
         switch(lastClicked){
-            case 0:;
-                addNumber("number1", parseInt(content))
+            case 0:
+                addNumber("number1", content);
                 break;
             case 1:
-                addNumber("number2", parseInt(content))
+                addNumber("number2", content);
                 break;
             case 2:
-                addNumber("number2", parseInt(content))
+                addNumber("number2", content);
                 lastClicked = 1;
                 break;
         }
@@ -98,34 +116,46 @@ function buttonClicked(e){
                 lastClicked = 0;
                 break;
             case "DEL":
-
+                if(lastClicked == 0){
+                    delNumber("number1");
+                }
+                else if (lastClicked == 1){
+                    delNumber("number2");
+                }
                 break;
             case "/":
-                if(lastClicked == 1) operate(calculator.number1, calculator.number2, calculator.operator);
+                if(lastClicked == 1) operate(parseFloat(calculator.number1), parseFloat(calculator.number2), calculator.operator);
                 calculator.operator = 4;
                 lastClicked = 2;
                 break;
             case "*":
-                if(lastClicked == 1) operate(calculator.number1, calculator.number2, calculator.operator);
+                if(lastClicked == 1) operate(parseFloat(calculator.number1), parseFloat(calculator.number2), calculator.operator);
                 calculator.operator = 3;
                 lastClicked = 2;
                 break;
             case "-":
-                if(lastClicked == 1) operate(calculator.number1, calculator.number2, calculator.operator);
+                if(lastClicked == 1) operate(parseFloat(calculator.number1), parseFloat(calculator.number2), calculator.operator);
                 calculator.operator = 2;
                 lastClicked = 2;
                 break;
             case "+":
-                if(lastClicked == 1) operate(calculator.number1, calculator.number2, calculator.operator);
+                if(lastClicked == 1) operate(parseFloat(calculator.number1), parseFloat(calculator.number2), calculator.operator);
                 calculator.operator = 1;
                 lastClicked = 2;
                 break;
             case "=":
-                if(lastClicked == 1)operate(calculator.number1, calculator.number2, calculator.operator);
-                lastClicked = 2;
+                if(lastClicked == 1){
+                    operate(parseFloat(calculator.number1), parseFloat(calculator.number2), calculator.operator);
+                    lastClicked = 2;
+                }
                 break;
             case ".":
-
+                if (lastClicked == 0){
+                    addDot("number1");
+                }
+                else if(lastClicked == 1){
+                    addDot("number2");
+                }
                 break;
         }
     }
